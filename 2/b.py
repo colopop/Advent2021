b49be7e5e94baa5d6d1526bf1a7a1d4235debde5
colopop <seberg94@gmail.com>
2021-12-02 02:22:19 -0500

    Day 2 solutions.

---
+++ 2/b.py
@@ -0,0 +1,13 @@
+with open("input.txt") as inp:
+    route = [(line.split()[0], int(line.split()[1])) for line in inp]
+
+pos = (0,0,0)
+for direction in route:
+    if direction[0] == "forward":
+        pos = (pos[0] + direction[1], pos[1] + pos[2] * direction[1], pos[2])
+    elif direction[0] == "down":
+        pos = (pos[0], pos[1], pos[2] + direction[1])
+    else:
+        pos = (pos[0], pos[1], pos[2] - direction[1])
+
+print(pos, pos[0] * pos[1])
