d1cc48f69e8ca95b33612c36f1074b82df2ce1e9
colopop <seberg94@gmail.com>
2021-12-05 16:41:33 -0500

    Day 5: Improved solutions.

--- 5/a.py
+++ 5/a.py
@@ -1,28 +1,19 @@
 with open("input.txt") as inp:
-    points = set()
-    duplicates = set()
-    for line in inp:
-        line = line.split()
-        x1, y1 = line[0].split(',')
-        x2, y2 = line[2].split(',')
-        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-        if x1 == x2:
-            #search vertically
-            direction = (y2-y1)//abs(y2-y1)
-            for y in range(y1, y2 + direction, direction):
-                print(x1, y)
-                if (x1, y) in points:
-                    duplicates.add((x1, y))
-                else:
-                    points.add((x1, y))
-        elif y1 == y2:
-            #search horizontally
-            direction = (x2-x1)//abs(x2-x1)
-            for x in range(x1, x2 + direction, direction):
-                print(x, y1)
-                if (x, y1) in points:
-                    duplicates.add((x, y1))
-                else:
-                    points.add((x, y1))
-    print(len(duplicates))
+    segments = [(int(line.split()[0].split(',')[0]), int(line.split()[0].split(',')[1]), 
+                 int(line.split()[2].split(',')[0]), int(line.split()[2].split(',')[1])) for line in inp]
 
+points = set()
+duplicates = set()
+for x1, y1, x2, y2 in segments:
+    x_direction = (x2-x1)//abs(x2-x1) if x1 != x2 else 0
+    y_direction = (y2-y1)//abs(y2-y1) if y1 != y2 else 0
+    x, y = x1, y1
+    while (x,y) != (x2 + x_direction, y2 + y_direction):
+        if (x,y) in points:
+            duplicates.add((x,y))
+        else:
+            points.add((x,y))
+        x += x_direction
+        y += y_direction
+
+print(len(duplicates))
--- 5/b.py
+++ 5/b.py
@@ -1,21 +1,19 @@
 with open("input.txt") as inp:
-    points = set()
-    duplicates = set()
-    for line in inp:
-        line = line.split()
-        x1, y1 = line[0].split(',')
-        x2, y2 = line[2].split(',')
-        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-        x_direction = (x2-x1)//abs(x2-x1) if x1 != x2 else 0
-        y_direction = (y2-y1)//abs(y2-y1) if y1 != y2 else 0
-        x, y = x1, y1
-        while (x,y) != (x2 + x_direction, y2 + y_direction):
-            if (x,y) in points:
-                duplicates.add((x,y))
-            else:
-                points.add((x,y))
-            x += x_direction
-            y += y_direction
+    segments = [(int(line.split()[0].split(',')[0]), int(line.split()[0].split(',')[1]), 
+                 int(line.split()[2].split(',')[0]), int(line.split()[2].split(',')[1])) for line in inp]
 
-    print(len(duplicates))
+points = set()
+duplicates = set()
+for x1, y1, x2, y2 in segments:
+    x_direction = (x2-x1)//abs(x2-x1) if x1 != x2 else 0
+    y_direction = (y2-y1)//abs(y2-y1) if y1 != y2 else 0
+    x, y = x1, y1
+    while (x,y) != (x2 + x_direction, y2 + y_direction):
+        if (x,y) in points:
+            duplicates.add((x,y))
+        else:
+            points.add((x,y))
+        x += x_direction
+        y += y_direction
 
+print(len(duplicates))
